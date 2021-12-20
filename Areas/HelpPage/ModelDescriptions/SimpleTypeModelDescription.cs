dd7d46e6145d8c0cbccdebc6fbb0df4bee483fd8
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-20 09:11:04 +0000

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NyamNyamWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NyamNyamWebAPI.WebApiApplication" Language="C#" %>
