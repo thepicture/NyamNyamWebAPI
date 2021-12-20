ea3ce26072e89f332c333abd58c3fa28962ecd61
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-20 09:49:13 +0000

    Add a dish view and a dish details view

---
+++ Controllers/DishesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NyamNyamWebAPI.Models.Entities;
+
+namespace NyamNyamWebAPI.Controllers
+{
+    public class DishesController : Controller
+    {
+        private NyamNyamEntities db = new NyamNyamEntities();
+
+        // GET: Dishes
+        public ActionResult Index()
+        {
+            var dish = db.Dish.Include(d => d.Category);
+            return View(dish.ToList());
+        }
+
+        // GET: Dishes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dish.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name");
+            return View();
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,BaseServingsQuantity,CategoryId,Image,RecipeLink,Description,FinalPriceInCents")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dish.Add(dish);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", dish.CategoryId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dish.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", dish.CategoryId);
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,BaseServingsQuantity,CategoryId,Image,RecipeLink,Description,FinalPriceInCents")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dish).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", dish.CategoryId);
+            return View(dish);
+        }
+
+        // GET: Dishes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dish.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Dish dish = db.Dish.Find(id);
+            db.Dish.Remove(dish);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
