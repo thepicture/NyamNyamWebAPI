bb2976adfde425350368f5ca5110b53219f2236d
thepicture <85333076+thepicture@users.noreply.github.com>
2021-12-29 08:28:17 +0000

    Add the mehod to finish an ordered dish

--- Controllers/CookingStagesController.cs
+++ Controllers/CookingStagesController.cs
@@ -1,10 +1,7 @@
 ﻿using NyamNyamWebAPI.Models.Entities;
 using NyamNyamWebAPI.Models.ResponseModels;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -14,12 +11,6 @@ namespace NyamNyamWebAPI.Controllers
     public class CookingStagesController : ApiController
     {
         private NyamNyamEntities db = new NyamNyamEntities();
-
-        // GET: api/CookingStages
-        public IQueryable<CookingStage> GetCookingStage()
-        {
-            return db.CookingStage;
-        }
 
         // GET: api/CookingStages?dishId=1&orderId=1
         [ResponseType(typeof(List<ResponseCookingStage>))]
@@ -42,70 +33,23 @@ namespace NyamNyamWebAPI.Controllers
             });
         }
 
-        // PUT: api/CookingStages/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutCookingStage(int id, CookingStage cookingStage)
+        // GET api/<controller>/Finish?dishId=1&orderId=1
+        [Route("api/CookingStages/Finish")]
+        [HttpGet]
+        public IHttpActionResult IncrementQuantity(int? dishId, int? orderId)
         {
-            if (!ModelState.IsValid)
+            if (dishId == null || orderId == null)
             {
-                return BadRequest(ModelState);
+                return BadRequest("attempt to make finishing of non-existing ordered dish");
             }
-
-            if (id != cookingStage.Id)
+            OrderedDish orderedDish = db.OrderedDish.Find(new object[] { orderId, dishId});
+            if (orderedDish == null)
             {
-                return BadRequest();
+                return BadRequest("the given ordered dish was not found in the db");
             }
-
-            db.Entry(cookingStage).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CookingStageExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/CookingStages
-        [ResponseType(typeof(CookingStage))]
-        public async Task<IHttpActionResult> PostCookingStage(CookingStage cookingStage)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.CookingStage.Add(cookingStage);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = cookingStage.Id }, cookingStage);
-        }
-
-        // DELETE: api/CookingStages/5
-        [ResponseType(typeof(CookingStage))]
-        public async Task<IHttpActionResult> DeleteCookingStage(int id)
-        {
-            CookingStage cookingStage = await db.CookingStage.FindAsync(id);
-            if (cookingStage == null)
-            {
-                return NotFound();
-            }
-
-            db.CookingStage.Remove(cookingStage);
-            await db.SaveChangesAsync();
-
-            return Ok(cookingStage);
+            orderedDish.EndCookingDT = System.DateTime.Now;
+            db.SaveChanges();
+            return Ok();
         }
 
         protected override void Dispose(bool disposing)
@@ -115,11 +59,6 @@ namespace NyamNyamWebAPI.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool CookingStageExists(int id)
-        {
-            return db.CookingStage.Count(e => e.Id == id) > 0;
         }
     }
 }
